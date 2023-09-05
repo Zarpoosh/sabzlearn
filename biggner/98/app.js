704b420417ca5ab77fa80d7b3ab717c3b3176869
Zarpoosh <mzar67839@gmail.com>
2023-09-05 18:21:17 +0330

    files commit

---
+++ biggner/98/app.js
@@ -0,0 +1,21 @@
+// فقط تعریف متغییر ها با var هوستینگ hoisting میشه
+// مقدار دهی متغییر ها hoist نمیشه
+
+// num1 = 10;
+// num2 = 20;
+// console.log(num1);
+// var num1, num2;
+
+////////////////////////////////////////////////////
+
+showValue()
+// function showValue(){
+//     console.log("minoo")
+// }        // این توابع hoisting میشن
+
+
+
+// var showValue=function(){
+//     console.log("object")
+// }        //توابعی که داخل متغییر ذخبره میشن hoist نمیشن
+
