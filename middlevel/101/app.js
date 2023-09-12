83c16f552ec2096405ad172eb22ae912b417a308
Zarpoosh <mzar67839@gmail.com>
2023-09-12 12:53:26 +0330

    101 commit

---
+++ middlevel/101/app.js
@@ -0,0 +1,20 @@
+// id
+let h1Elem = document.getElementById("title");
+console.log(h1Elem);
+
+//class  => giv us array
+let liElems = document.getElementsByClassName("list-item");
+console.log(liElems[2]);
+
+// tag name
+let Items = document.getElementsByTagName("li");
+console.log(Items);
+
+// query selector  =>  first element
+let firstListItem = document.querySelector(".list-item");
+console.log(firstListItem);
+
+
+// all elements
+let elements = document.querySelectorAll(".list-item");
+console.log(elements);
