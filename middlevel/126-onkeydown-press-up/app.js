c74863a8a608071696623e4f1a72616b7e1363cb
Zarpoosh <mzar67839@gmail.com>
2023-09-29 18:08:46 +0330

    124, 125 ,126 commit

---
+++ middlevel/126-onkeydown-press-up/app.js
@@ -0,0 +1,16 @@
+function keyDownHandler() {
+  console.log("down");
+}
+
+function keyPressHandler() {
+  console.log("press");
+}
+
+function keyUpHandler() {
+  console.log("up");
+}
+
+
+
+// dont have keypress : alt / shift / F1 ,F2 ,...
+// اگه دستمون رو کلید باشه up رخ نمیده
