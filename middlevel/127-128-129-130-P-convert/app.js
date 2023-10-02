6c44fd29af4e56e1aa0a4a7f5e99431222c31f3d
Zarpoosh <mzar67839@gmail.com>
2023-10-02 21:01:11 +0330

    fised bug commit

--- middlevel/127-128-129-130-P-convert/app.js
+++ middlevel/127-128-129-130-P-convert/app.js
@@ -4,23 +4,28 @@ let inputBox = $.querySelector("#input");
 let convertBtn = $.querySelector("#btn1");
 let resetBtn = $.querySelector("#btn2");
 let swapBtn = $.querySelector("#btn3");
-let firstValue = $.querySelector("#C");
+let firstValue = $.querySelector("#C"); // // ==> span not Value
 let secondValue = $.querySelector("#F");
-let result = document.querySelector(".result");
+let result = document.querySelector(".result"); //مینو  هستی اره
 
 function convert() {
   if (inputBox.value === "") {
     result.innerHTML = "Enter a number";
     result.style.color = "red";
+    console.log("1=> if  fun convert");
   } else {
-    if (firstValue.value === "C") {
-      // console.log("resultValue");
+    console.log("1=> else  fun convert"); //
+    console.log(firstValue.value, "firstValue"); // Element Span not value underStand   ==>  undefined  <==
+    console.log(firstValue.innerHTML, "firstValue"); // Element Span not value underStand ===> span is it innerHtml <span >innerHtml not value  not in  input</span>   ==>   undefind  <==
+    // inputBox.innerHTML  ==> <input  value=""  type=""  placeHolder=""  />
+    // bug fixFinsh
+
+    if (firstValue.innerHTML === "C") {
       let resultValue = inputBox.value * 1.8 + 32;
       result.innerHTML =
         inputBox.value + " C to " + resultValue.toFixed(2) + " F ";
       result.style.color = "green";
     } else {
-      // console.log("object")
       let resultValue = ((inputBox.value - 32) * 5) / 9;
       result.innerHTML =
         inputBox.value + " F to " + resultValue.toFixed(2) + " C ";
@@ -30,21 +35,24 @@ function convert() {
 }
 
 function reset() {
+  console.log("1=> if  fun rest");
   inputBox.value = "";
   result.innerHTML = "";
 }
 
 function swap() {
-  if (firstValue.innerHTML === "C") {
+  if (inputBox.placeholder === "C") {
     firstValue.innerHTML = "F";
     secondValue.innerHTML = "C";
     inputBox.setAttribute("placeholder", "F");
     document.title = "F to C";
+    console.log("1=> if F to C");
   } else {
     firstValue.innerHTML = "C";
     secondValue.innerHTML = "F";
     inputBox.setAttribute("placeholder", "C");
     document.title = "C to F";
+    console.log("1=> else  C to F");
   }
 }
 
---
+++ middlevel/131-difrence-between-prop-atribute/main.js
@@ -0,0 +1,10 @@
+let h1Elem = document.getElementById("title");
+let userNameInput = document.getElementById("input");
+
+// get attribiut
+console.log(h1Elem.getAttribute("class"));
+
+// .
+console.log(h1Elem.className);
+console.log(userNameInput.placeholder)
+
