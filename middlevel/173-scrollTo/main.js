3dab299b19372c80f2483562b157109610f834bd
Zarpoosh <mzar67839@gmail.com>
2023-11-08 15:11:26 +0330

    change name

--- middlevel/173-scrollTo/main.js
+++ middlevel/173-scrollTo/main.js
@@ -1,8 +1,10 @@
 function scrolllTOpHandler(){
+    console.log("scrollto")
     window.scrollTo(0,0)
 }
 
 function scrolllBypHandler(){
+    console.log("scrollby")
     window.scrollBy(0, 100)
 }
 
