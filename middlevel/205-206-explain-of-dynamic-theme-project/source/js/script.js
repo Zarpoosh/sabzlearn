97ad073d6bf5d2f2486f9125994478e72f0bc57a
Zarpoosh <mzar67839@gmail.com>
2023-11-22 14:32:51 +0330

    205-206

---
+++ middlevel/205-206-explain-of-dynamic-theme-project/source/js/script.js
@@ -0,0 +1,10 @@
+const btn=document.querySelectorAll(".btn")
+
+
+btn.forEach(function (theme){
+    console.log(theme)
+    theme.addEventListener("click",function(e){
+        let btnColor=e.target.dataset.color
+        document.documentElement.style.setProperty("--theme-color",btnColor)
+    })
+})
