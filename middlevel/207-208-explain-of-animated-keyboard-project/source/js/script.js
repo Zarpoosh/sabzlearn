64fd89c05118fd414602b8302b7704b286586066
Zarpoosh <mzar67839@gmail.com>
2023-11-24 18:13:06 +0330

    209

--- middlevel/207-208-explain-of-animated-keyboard-project/source/js/script.js
+++ middlevel/207-208-explain-of-animated-keyboard-project/source/js/script.js
@@ -1,6 +1,8 @@
 const $=document;
+const titleElem=$.querySelector(".title");
 document.addEventListener("keyup" ,function(e){
     // console.log(e.key)
+    appendValueToDom(e)
     let  userEventKey=e.key.toUpperCase()  //it has to be uppercase
     let mainKeyElem=document.getElementById(userEventKey)
     
@@ -11,3 +13,12 @@ document.addEventListener("keyup" ,function(e){
     })
     console.log(mainKeyElem)
 })
+
+function appendValueToDom(e){
+    // console.log(e)
+    if (e.code==="Backspace"){
+        titleElem.innerHTML=titleElem.innerHTML.slice(0,-1)
+        return
+    }
+    titleElem.innerHTML+=e.key
+}
