ef95a2f8b16fb564bf04ec0782e11ccbb0bea6f7
Zarpoosh <mzar67839@gmail.com>
2023-12-15 12:29:17 +0330

    230, 231,  232

---
+++ middlevel/230-mouse-events/main.js
@@ -0,0 +1,22 @@
+function mouseDownHandler(){
+    console.log("mouseDownHandler");
+}
+function onmouseupHandler(){
+    console.log("onmouseupHandler");
+}
+function onmouseEnterHandler(){
+    console.log("mouse entr");
+}
+function onmouseleaveHandler(){
+    console.log("leave");
+}
+function onmouseoverHandler(){
+    console.log("mouse onmouseoverHandler");
+}
+function onmouseoutHandler(){
+    console.log("onmouseoutHandler");
+}
+function onmousemoveHandler(event){
+    console.log(event);
+    console.log("onmousemoveHandler");
+}
---
+++ middlevel/231-insertAdjacentHtml/main.js
@@ -0,0 +1,8 @@
+// Element.insertadjacment(position, text or cpodes)
+
+const box=document.querySelector(".box")
+
+// box.insertAdjacentHTML("afterbegin", '<li>minoo</li>')
+// box.insertAdjacentHTML("beforebegin", '<li>minoo</li>')
+// box.insertAdjacentHTML("beforeend", '<li>minoo</li>')
+box.insertAdjacentHTML("afterend", '<li>minoo</li>')
