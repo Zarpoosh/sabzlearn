c0754033ba7be5b201f79cc59e72c6672832efe6
Zarpoosh <mzar67839@gmail.com>
2023-12-16 16:09:13 +0330

    concol methodes

--- middlevel/236-consol-methodes/main.js
+++ middlevel/236-consol-methodes/main.js
@@ -23,10 +23,33 @@ const user = {
 };
 console.table(user);
 
-const product = [
-  { id: 1, name: "laptopb", price: 29900 },
-  { id: 2, name: "phone", price: 32300 },
-  { id: 3, name: "handsfere", price: 120000 },
-  { id: 4, name: "ssd card", price: 76000 },
-];
-console.table(product);
+// const product = [
+//   { id: 1, name: "laptopb", price: 29900 },
+//   { id: 2, name: "phone", price: 32300 },
+//   { id: 3, name: "handsfere", price: 120000 },
+//   { id: 4, name: "ssd card", price: 76000 },
+// ];
+// console.table(product);
+
+// ------------------------------------------------------------
+
+// بستگی به رم و مغز کامپیوتر و....
+console.time("For loop")
+for(let i = 0; i <1000; i++){
+}
+console.timeEnd("For loop");
+// --------------------------------------------
+
+console.log("قبل از گروه-ابتدا")
+
+console.group("first logs")
+console.log("1 log")
+console.log("2 log")
+console.log("3 log")
+console.groupEnd("first logs")
+
+console.log("بعد از گروه-انتها")
+// --------------------------------------------
+
+//  %c , %s ,\$
+console.log("%c its wrong !", "color:green;")
