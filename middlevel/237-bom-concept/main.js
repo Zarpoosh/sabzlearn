b446ec9fac6adf4aef0eaa9d5f7464c2d5186211
Zarpoosh <mzar67839@gmail.com>
2023-12-16 17:19:56 +0330

    bom & dom

---
+++ middlevel/237-bom-concept/main.js
@@ -0,0 +1,9 @@
+// Dom =>> document object model
+// Bom=>> browser object model
+
+// window.alert("helllo")
+
+
+let h1Elem=window.document.getElementById("title")
+window.console.log(h1Elem)
+window.console.log("logger")
