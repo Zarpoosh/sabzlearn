abe2bf9c0a0c79f5c5071900a1ee6a6e4aa33022
Zarpoosh <mzar67839@gmail.com>
2023-12-17 18:37:59 +0330

    243 location

---
+++ middlevel/243-location-object/main.js
@@ -0,0 +1,8 @@
+console.log(location)
+console.log(location.protocol)
+console.log(location.hostname)  //domain
+console.log(location.pathname)
+console.log(location.search)
+console.log(location.hash)
+console.log(location.href)
+
