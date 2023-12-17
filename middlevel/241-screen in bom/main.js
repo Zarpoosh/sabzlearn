76489b81d03b0d3afdc72fa0f353fd1899cfb2fc
Zarpoosh <mzar67839@gmail.com>
2023-12-17 18:02:18 +0330

    241 screen

---
+++ middlevel/241-screen in bom/main.js
@@ -0,0 +1,15 @@
+// screen in bomclg
+// global
+console.log(screen)
+console.log(window.screen)
+console.log(screen.availWidth)
+console.log(screen.width)
+
+
+// تعداد بیت  هایی که این نمایشگر برای نمایش یک  نقطه ساتفاده میکنه
+console.log(screen.pixelDepth)
+
+// تعداد بیت  هایی که این نمایشگر برای نمایش یک پیکسل از هر رنگ استفاده میکنه
+console.log(screen.colorDepth)
+
+
