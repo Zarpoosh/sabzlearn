a1ea6ea316b50b868b3ccb10fbb9e9bf6dbc21f8
Zarpoosh <mzar67839@gmail.com>
2023-12-18 14:04:09 +0330

    248

---
+++ middlevel/248/main.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#reload");
+
+btn.addEventListener("click", function () {
+//   console.log("reload");
+  location.reload()
+  location.assign()
+  location.replace()
+});
