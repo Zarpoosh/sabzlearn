f97c696b2a4cb264f97428e09e1afb97dc80551d
Zarpoosh <mzar67839@gmail.com>
2024-01-15 12:57:53 +0330

    256-console-pro-tips

---
+++ advance/256-console-pro-tips/main.js
@@ -0,0 +1,14 @@
+$("#hello").addEventListener("click", function () {
+  console.log("click");
+});
+
+$0.style.color="red"
+
+//TODO ==> document.designMode = "on/off"
+//TODO ==> document.body.contentEditable=true /false;
+
+
+
+// * $_   ====> دسترسی به جواب کد قبلی در کنسول
+//? $ ===> document.querySelector
+//! $$ ===> document.querySelectorall
