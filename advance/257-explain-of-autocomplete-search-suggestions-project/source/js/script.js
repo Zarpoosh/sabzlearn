85b92499a6975592c45e6e7795442dd90d940388
Zarpoosh <mzar67839@gmail.com>
2024-01-16 11:30:23 +0330

    258

--- advance/257-explain-of-autocomplete-search-suggestions-project/source/js/script.js
+++ advance/257-explain-of-autocomplete-search-suggestions-project/source/js/script.js
@@ -1,19 +1,57 @@
 const $ = document;
 
-const aoutoCompletRapper = $.querySelector(".search-input");
+const autoCompletewrapper = $.querySelector(".search-input");
 const searchInputElem = $.querySelector("input");
-const aoutoCompletBox = $.querySelector(".autocom-box");
+const autoCompletBox = $.querySelector(".autocom-box");
 
 searchInputElem.addEventListener("keyup", function () {
-  let saerchValue = searchInputElem.value;
-  if (saerchValue) {
-    aoutoCompletRapper.classList.add("active");
-    let filterdWords = suggestions.filter(function (word) {
-      return word.toLowerCase().include(saerchValue.toLowerCase());
+  let searchValue = searchInputElem.value;
+
+  if (searchValue) {
+    autoCompletewrapper.classList.add("active");
+    let filteredWords = suggestions.filter(function (word) {
+      //! includes  ==> all of words that have the letter
+      //TODO instead includes we write startsWith
+      return word.toLowerCase().startsWith(searchValue.toLowerCase());
     });
 
-    console.log(filterdWords)
+
+    suggestionWordsGenerator(filteredWords);
+
+    console.log(filteredWords);
   } else {
-    aoutoCompletRapper.classList.remove("active");
+    autoCompletewrapper.classList.remove("active");
   }
 });
+
+
+function suggestionWordsGenerator(wordArray){
+  let listItemsArray=wordArray.map(function(word){
+    return '<li>' +word+ '</li>'
+  })
+
+  console.log(listItemsArray);
+
+  let customeListItem; 
+  if (listItemsArray.length) {
+    customeListItem = '<li>' + searchInputElem.value + '</li>';
+  } else {
+    customeListItem = listItemsArray.join("");
+  }
+
+
+  autoCompletBox.innerHTML=customeListItem
+  select()
+  console.log(customeListItem);
+}
+
+
+function select(){
+  let allListItems = autoCompletBox.querySelectorAll("li")
+  allListItems.forEach(function(wordItem){
+    wordItem.addEventListener("click", function(event){
+      searchInputElem.value=event.target.textContent
+      autoCompletewrapper.classList.remove("active");
+    })
+  })
+}
