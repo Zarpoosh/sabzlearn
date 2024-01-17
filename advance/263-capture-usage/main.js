ddf816f8295a1e0d7c14fe8f0d601db9b251f327
Zarpoosh <mzar67839@gmail.com>
2024-01-17 17:39:31 +0330

    263, capture usage

---
+++ advance/263-capture-usage/main.js
@@ -0,0 +1,31 @@
+const listItem = document.querySelectorAll("li");
+const inputElem = document.querySelector("input");
+const ulElem = document.querySelector(".list");
+
+// listItem.forEach(function (item) {
+//   item.addEventListener("click", function (event) {
+//     console.log(event.target);
+//     event.target.remove();
+//   });
+// });
+
+ulElem.addEventListener("click", function(e){
+    // console.log(e.target.tagName);
+
+    if (e.target.tagName==="LI"){
+        e.target.remove()
+    }
+})
+
+
+inputElem.addEventListener("keypress", function (e) {
+  if (e.keyCode == 13) {
+    // console.log("enter press")
+    let newLanguageLi = document.createElement("li");
+    newLanguageLi.innerHTML = e.target.value;
+
+    ulElem.append(newLanguageLi);
+    console.log(listItem)
+    e.target.value=""
+  }
+});
