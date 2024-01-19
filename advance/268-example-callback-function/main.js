dc474a42fe3ad859f8cec64d547b8354c173fb02
Zarpoosh <mzar67839@gmail.com>
2024-01-19 13:26:38 +0330

    Zarpoosh_#2_268_explain call back function

---
+++ advance/268-example-callback-function/main.js
@@ -0,0 +1,27 @@
+let books = [
+  { id: 1, name: "book1", price: 2000 },
+  { id: 2, name: "book2", price: 6500 },
+  { id: 3, name: "book3", price: 26500 },
+  { id: 4, name: "book4", price: 82000 },
+];
+
+
+function addBook(name, price, callback) {
+    let newBook ={
+        id:books.length+1, 
+        name: name,
+        price: price
+    }
+
+    setTimeout(function() {
+        books.push(newBook);
+        callback()
+    }, 4000);
+}
+
+
+function logBooks() {
+    console.log(books);
+}
+
+addBook("minoo", 54000, logBooks)
