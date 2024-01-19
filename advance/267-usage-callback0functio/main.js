6a5798d1724ca0fe516b9e78d7fb4ccb1385185e
Zarpoosh <mzar67839@gmail.com>
2024-01-19 12:56:45 +0330

    Zarpoosh_#1_267_usage

---
+++ advance/267-usage-callback0functio/main.js
@@ -0,0 +1,44 @@
+//! ---------------- asynchronous ------------------
+
+// function firstLog() {
+//   console.log(1);
+//   console.log(2);
+// }
+
+// function secondLog() {
+//   setTimeout(function () {
+//     console.log(3);
+//   }, 5000);
+// }
+
+// function lastLog() {
+//   console.log(4);
+//   console.log(5);
+// }
+
+// firstLog();
+// secondLog();
+// lastLog();
+
+//! --------------- synchronous ---------------------
+
+function firstLog() {
+  console.log(1);
+  console.log(2);
+}
+
+function secondLog(callback) {
+  setTimeout(function () {
+    console.log(3);
+    callback()
+  }, 5000);
+
+}
+
+function lastLog() {
+  console.log(4);
+  console.log(5);
+}
+
+firstLog();
+secondLog(lastLog);
