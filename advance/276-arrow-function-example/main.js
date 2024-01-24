ee374252c2ea3359b92a98c44978fe22cedae6c4
Zarpoosh <mzar67839@gmail.com>
2024-01-24 14:08:54 +0330

    arrow function example 276

---
+++ advance/276-arrow-function-example/main.js
@@ -0,0 +1,16 @@
+// arrow function
+
+let numbers = [1, 2, 3, 4, 55, 75, 23];
+
+// numbers.forEach((num)=>console.log(num))
+
+let mapData = (numbers = numbers.map((num) => num * 2));
+console.log(mapData);
+
+
+// ----------------------------arrow func in addEventListener-------------------------
+
+const btn=document.querySelector('#btn');
+btn.addEventListener("click", (e) =>{
+    console.log(e)
+})
