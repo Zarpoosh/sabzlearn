91555cc5e2318a4a580b96887ee4a5f7d80f30dc
Zarpoosh <mzar67839@gmail.com>
2024-01-24 12:42:43 +0330

    strict mod

---
+++ advance/273-strict-mod/main.js
@@ -0,0 +1,26 @@
+// strict mod ---->"use strict"
+"use strict";
+
+//*--------------------- example 1 -----------------
+// number = 12;
+// console.log(number);
+
+//*--------------------- example 2 -----------------
+// function showValue(a,a) {
+  
+// }
+//*--------------------- example 3 -----------------
+//! نمیتونیم ولیو های readonly رو مقدار دهی کنیم
+// NaN=12
+// undefined=1123
+//*--------------------- example 4 -----------------
+// console.log(020 + 010) //!octal
+
+//*--------------------- example 5 -----------------
+//!cant use eval method in strict mod
+// let eval=12
+
+//*--------------------- example 6 -----------------
+//! cant use delete the variables in strict mod
+// let username="minoo"
+// delete username
