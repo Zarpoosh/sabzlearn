e08ca67f432ce99607e3697fba8490655c5d9aae
Zarpoosh <mzar67839@gmail.com>
2024-01-28 19:53:32 +0330

    277

---
+++ advance/277-/main.js
@@ -0,0 +1,35 @@
+//spread syntax - just itterable (array, string)
+
+let numbers = [1, 2, 3, 4, 5, 56];
+
+// console.log(numbers[0], numbers[2])
+// numbers.forEach(num =>console.log(num));
+
+//------------------------------------------------------------
+
+// let copyNumbers=numbers
+// copyNumbers.push(29)
+
+// //! we cant push
+// // console.log("number :", numbers)
+// // console.log("number: ", copyNumbers)
+
+//----------------------------- spread in array -------------------------------
+// console.log(...numbers)
+
+let copyNumbers = [...numbers];
+copyNumbers.push(29);
+
+//? we can push
+// console.log("number :", numbers)
+// console.log("number: ", copyNumbers)
+
+let nums1 = [1, 2, 3, 4];
+let nums2 = [5, 6, 7, 8];
+let allNumbers = [...nums1, ...nums2 , 54,9868];
+console.log(allNumbers);
+
+//------------------------------------------------------
+function myFunction(x, y, z) {}
+const args = [0, 1, 2];
+myFunction(...args);
