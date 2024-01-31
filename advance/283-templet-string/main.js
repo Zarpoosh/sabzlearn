b25ae03f72e560aa87dd788b7ffaa9721261de25
Zarpoosh <mzar67839@gmail.com>
2024-01-31 10:45:36 +0330

    283

---
+++ advance/283-templet-string/main.js
@@ -0,0 +1,19 @@
+// let type="fake"
+// let txt = "lorem ipsum " + type
+// console.log(txt)
+
+//////////////////////////////////////////////
+
+// template string (literal)
+
+let type="fake"
+let txt = `lorem ipsum ${type} =>${Math.pow(2,3)}`
+
+let name=`minoo
+kimia
+`
+ console.log(txt)
+ console.log(name)
+
+
+//  document.body.insertAdjacentHTML
