c85f77e0a1d77a2a11814004d761a31a31ecad6b
Zarpoosh <mzar67839@gmail.com>
2024-02-04 20:30:35 +0330

    286

---
+++ advance/286-string-padding/main.js
@@ -0,0 +1,20 @@
+// string paddding 
+
+let userName="minoo"
+
+
+//default is space
+console.log(userName.padStart(6, "*"))
+
+//////////////////////////////////////////////////////
+// let size="8"
+// console.log(size.padStart(2,"0"))
+
+///////////////////////////////////////////////////
+
+let phonNumber="09940002432"
+
+let last4Digits=phonNumber.slice(-4)
+// console.log(last4Digits)
+let finalNumber=last4Digits.padStart(phonNumber.length, "*")
+console.log(finalNumber)
