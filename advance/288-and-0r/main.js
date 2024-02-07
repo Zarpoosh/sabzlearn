c7e8f9885c8a3f94c8d3c95eb04bb1b3fa510a20
Zarpoosh <mzar67839@gmail.com>
2024-02-07 13:50:15 +0330

    289 or

--- advance/288-and-0r/main.js
+++ advance/288-and-0r/main.js
@@ -1,17 +1,43 @@
 // && (and)  || (or)
 
-let userAge=10
+//--------------------------------------   AND -------------------------------------------------------------
+let userAge=13
 // if(userAge>18){
 //     alert("you login succesfully!")
 // }
 
 
-userAge >18 && alert("you login succesfully!")
+// userAge >18 && alert("you login succesfully!")
+
+// userAge >18 ? alert("login successfully! :)") : alert("surry you cant login ! :(")
+
+//-------------------------------------------  OR --------------------------------------------------------
 
 
-userAge >18 ? alert("login successfully! :)") : alert("surry you cant login ! :(")
+//------>false     true پس دومی رو اجرا میکنه
+// userAge !==13 || alert("hello! :)")
 
-// true && true ==>true
-// false && false ==> false
-// false && true ==> false
-// true && false ==> false
+//---->true پس همین اجرا میکنه و سراغ بعدی نمیره 
+userAge ===13 || alert("hello! :)")
+
+
+
+let usrType="admin"
+let userToken=usrType || "user" 
+
+console.log(userToken)
+
+
+
+//با اولین فالس میشه فالس
+//? true && true ==>true
+//? false && false ==> false
+//? false && true ==> false
+//? true && false ==> false
+
+
+// با اولین ترو میشه ترو
+//* true || true ==>true
+//* false || false ==> false
+//* false || true ==> true
+//* true || false ==> true
