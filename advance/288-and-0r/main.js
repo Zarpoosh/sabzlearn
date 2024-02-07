b24008d68178dff19dbe5c286c228b8d90782bf5
Zarpoosh <mzar67839@gmail.com>
2024-02-07 13:37:10 +0330

    288 and or in js

---
+++ advance/288-and-0r/main.js
@@ -0,0 +1,17 @@
+// && (and)  || (or)
+
+let userAge=10
+// if(userAge>18){
+//     alert("you login succesfully!")
+// }
+
+
+userAge >18 && alert("you login succesfully!")
+
+
+userAge >18 ? alert("login successfully! :)") : alert("surry you cant login ! :(")
+
+// true && true ==>true
+// false && false ==> false
+// false && true ==> false
+// true && false ==> false
