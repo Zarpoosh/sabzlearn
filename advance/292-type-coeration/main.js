7a895df1dcb1aa8f26d45ce9bac70994659fee10
Zarpoosh <mzar67839@gmail.com>
2024-02-11 18:21:28 +0330

    292 coeration

---
+++ advance/292-type-coeration/main.js
@@ -0,0 +1,5 @@
+//* type coeration ( implicit: اجباری - explicit: صریح و واضح ) -----> تبدیل نوع اجباری  ()
+
+let userAge = Number(prompt("enter your age: ", 10));
+console.log("userAge", userAge)
+console.log(typeof (userAge))
