778d64014b43914fdf9dcc3d8b081359780a52f0
Zarpoosh <mzar67839@gmail.com>
2024-03-10 22:26:33 +0330

    341-343

--- advance/341-get-users-from-db/content/js/app.js
+++ advance/341-get-users-from-db/content/js/app.js
@@ -1,29 +1,82 @@
-const userContainer=document.querySelector("#wrap-users")
+const usersContainer = document.querySelector('#wrap-users')
+const deleteModal=document.querySelector("#delete-modal")
+const editModal=document.querySelector("#edit-modal")
+const firstname=document.querySelector(".firstname")
+const lastname=document.querySelector(".lastname")
+const password=document.querySelector(".password")
+
+let userID=null
 
 
-window.addEventListener("load", ()=>{
-    // console.log("loaded");
+window.addEventListener('load', () => {
+   getAllUsers()
+})
 
-    fetch("http://localhost/3000/api/users")
-    .then(res=>res.json())
-    .then(data=>{
+function getAllUsers(){
+    fetch('http://localhost:3000/api/users')
+    .then(res => res.json())
+    .then(data => {
+        usersContainer.innerHTML=""
         data.forEach(user => {
-            // console.log(user);
-            userContainer.insertAdjacentHTML("beforeend",`
+            usersContainer.insertAdjacentHTML('beforeend', `
             <div class="user">
-            //                 <div class="user-profile-wrap">
-            //                     <img class="user-profile" src="content/img/noimg.png" alt="default-image">
-            //                     <div class="user-profile-description">
-            //                         <h1 class="user-profile-name">${user.firstname} - ${user.lastname}<span class="user-age">18</span> </h1>
-            //                         <h3 class="user-explanations">Pass: ${user.password}</h3>
-            //                     </div>
-            //                 </div>
-            //                 <div class="btn-groups-column">
-            //                     <button class="delete-user-btn">delete</button>
-            //                     <button class="edit-user-btn">edit</button>
-            //                 </div>
-            //             </div> 
-            ` )
+            <div class="user-profile-wrap">
+                <img class="user-profile" src="content/img/noimg.png" alt="default-image">
+                <div class="user-profile-description">
+                    <h1 class="user-profile-name">${user.firstname} - ${user.lastname}<span class="user-age">18</span> </h1>
+                    <h3 class="user-explanations">Pass: ${user.password}</h3>
+                </div>
+            </div>
+            <div class="btn-groups-column">
+                <button class="delete-user-btn" onclick="openDeleteModal('${user._id})">delete</button>
+                <button class="edit-user-btn" onclick="openEditModal('${user._id})>edit</button>
+            </div>
+        </div>
+            `)
         });
     })
-})
+}
+
+function openDeleteModal(){
+    // console.log("dalete");
+
+    usersContainer=id
+    deleteModal.classList.add("visible")
+}
+
+function closeModal(){
+    deleteModal.classList.remove("visible")
+}
+
+
+function openEditModal(){
+    userID=id
+    editModal.classList.add("visible")
+}
+
+
+function updateUser(){  //put
+    let userData={
+        firstname :firstname.value, 
+        lastname :lastname.value,
+        password :password.value
+    }
+    fetch(`https://localhost:3000/api/users/${userID}`, {
+        method:"PUT", 
+        headers:{
+            "Content-type": 'application/json'
+        },
+        body:JSON.stringify(userData)
+    })
+    .then(res=>console.log(res))
+}
+function deleteUser(){
+    fetch(`https://localhost:3000/api/users/${userID}` , {  //get
+        method:"DELETE", 
+    })  
+    .then(res=>res.json())
+    .then(data=>console.log(data)) 
+
+    getAllUsers()
+    closeModal()
+}
