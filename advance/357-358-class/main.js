96137f23da18bbfab473310f993cf6eec559e3f9
Zarpoosh <mzar67839@gmail.com>
2024-03-26 17:16:14 +0330

    359

--- advance/357-358-class/main.js
+++ advance/357-358-class/main.js
@@ -12,16 +12,54 @@ class Person {
     this.address = useraddress;
   }
 
-  startDev=()=> {
+  startDev (){
     console.log(`${this.name} start developing !`);
+  };
+  getAge(){
+    return this.age
   }
 }
 
-class Professor{
-  constructor(){
-    
+//TODO========>>> inheritanceارث بری ==>extends
+
+class Professor extends Person {
+  constructor(
+    username,
+    userjob,
+    userage,
+    useraddress,
+    userlessone,
+    yearsofjob
+  ) {
+    // this.name = username;
+    // this.job = userjob;
+    // this.age = userage;
+    // this.address = useraddress;
+
+    super(username, userjob, userage, useraddress);
+    this.name = username;
+    this.lessone = userlessone;
+    this.years = yearsofjob;
+
+
+    startDev = () => {
+      console.log(super.getAge())
+      console.log(`${this.name} start teaching !`);
+    };
   }
 }
+
+let prefossorMinoo = new Professor(
+  "minoo",
+  "fronenddeveloper",
+  20,
+  "ardabil",
+  "programming",
+  2
+);
+console.log("========================================");
+console.log(prefossorMinoo);
+console.log("========================================");
 
 let personalMinoo = new Person("minoo", "developer", 20, "ardabil"); //call فراخوانی
 let personalMohammad = new Person("mohammad", "developer", 32, "isfahan"); //call فراخوانی
@@ -32,5 +70,3 @@ console.log(personalMohammad.job);
 //----------------------------------------
 personalMinoo.startDev();
 console.log(Person.startDev()); //!  its wrong.نمیتونم قبل ساختن یه شی به متد هاش دسترسی داشته باشیم
-
-
