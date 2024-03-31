f7006bba7a3fc67b63f5226c7988e9939e5f3ee5
Zarpoosh <mzar67839@gmail.com>
2024-03-31 18:06:52 +0330

    369  reduce methode

---
+++ advance/369-reduce-method/main.js
@@ -0,0 +1,11 @@
+let number = [1, 2, 3, 4, 5, 6];
+
+// number.map();
+// number.filter();
+
+number.reduce((preValue, curentValue) => {
+  console.log(preValue, curentValue); // 1,2 |2,3 |3,4 | 4,5 | 5,6
+
+  //   return curentValue
+  return "minicode"; //1,2 |  minicode, 3 | minicode, 4 | minicode, 5 | minicode 6
+});
