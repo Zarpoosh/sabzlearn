357cfea743d32af581ddc7ae80047efc5aa62cd2
Zarpoosh <mzar67839@gmail.com>
2024-04-10 19:28:31 +0330

    388 get cookie

--- advance/386-387/js/main.js
+++ advance/386-387/js/main.js
@@ -1,21 +1,37 @@
-//TODO ===>> Cookies 
+//TODO ===>> Cookies
 
 // login ==> save paasword in cookie
 
-const setCookieBtn=document.querySelector("button")
+const setCookieBtn = document.querySelector("button");
+const getCookieBtn = document.querySelector("#get-cookie");
 
-setCookieBtn.addEventListener("click" , ()=>{
-    // console.log("cookie");
-    console.log(document.cookie);
+setCookieBtn.addEventListener("click", () => {
+  // console.log("cookie");
+  console.log(document.cookie);
 
+  //! ==> از سال ۱۹۷۰ به اینور این همه میلی ثانیه میگذره
+  const now = new Date();
+  // console.log(now.getTime());
 
-    //! ==> از سال ۱۹۷۰ به اینور این همه میلی ثانیه میگذره
-    const now=new Date()
-    // console.log(now.getTime());
+  console.log(now);
+  let expireDay = now.setTime(now.getTime() + 2 * 24 * 60 * 60 * 1000);
+  console.log(now);
 
-    console.log(now)
-    let expireDay=now.setTime(now.getTime()+ 2*24*60*60 *1000)
-    console.log(now)
+  document.cookie = `user-age=20 developer;path=/;expires${now}`;
+});
 
-    document.cookie=`user-name=minoo-zarpoosh;path=/;expires${now}`
-})
+getCookieBtn.addEventListener("click", () => {
+
+    let mainCookieName=prompt("enter the cookie name :)")
+  let cookiesArray = document.cookie.split(";");
+  let mainCookie=null;
+
+  cookiesArray.some((cookie) => {
+    if(cookie.includes(mainCookieName)){
+        mainCookie=cookie.substring(cookie.indexOf("="+1))
+        return true
+    }
+  })
+
+  console.log(mainCookie)
+});
