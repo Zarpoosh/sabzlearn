969cc1773f6bb93dfe7eaa0b5a78077e7bf9e5d0
Zarpoosh <mzar67839@gmail.com>
2024-04-12 21:52:09 +0330

    393/394/395

---
+++ advance/394-exc-with-promise/main.js
@@ -0,0 +1,52 @@
+const userLogin = (username, password) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        username: username,
+        password: password,
+        email: "mzar67839@mail.com",
+      });
+    }, 4000);
+  });
+};
+const userCourses = (username) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, title: "js course", price: 3_000_000 },
+        { id: 1, title: "js course", price: 3_000_000 },
+      ]);
+    }, 3000);
+  });
+};
+
+const mainCoursesInfo = (courseTitle) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ Pid: 1, title: "js course", price: 2000 });
+    }, 2000);
+  });
+};
+
+console.log("سایت لود ش !");
+
+// const loginInfos = userLogin("minoo zarpoosh", "86765", (userObject) => {
+//   console.log("کاربر لاگین شد !", userObject);
+//   userCourses(userObject.username, (userAllCourses) => {
+//     console.log("user courses");
+
+//     mainVideoInfo(userAllCourses[1].title, (mainInfos) => {
+//       console.log("main course infos : ", mainInfos);
+//     });
+//   });
+// });
+
+
+userLogin()
+.then(userObject => userCourses(userObject.username))
+.then(userAllCourses=>mainCoursesInfo(userAllCourses[1].title))
+.then(mainInfos=>console.log(mainInfos))
+
+// console.log("logined", loginInfos);
+
+console.log("کاربر با موفقیت لاگین شد ");
